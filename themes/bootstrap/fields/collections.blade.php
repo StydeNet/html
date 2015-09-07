8dc1db7e08a8db18f9032b1022ab261b829e561f
Dimitri Acosta <dimitri316@gmail.com>
2015-09-06 19:01:16 -0600

    Fix html templates style code

--- themes/bootstrap/fields/collections.blade.php
+++ themes/bootstrap/fields/collections.blade.php
@@ -3,10 +3,11 @@
 {!! $input !!}
 
 @if ( ! empty($errors))
-<div class="controls">
-    @foreach ($errors as $error)
-        <p class="help-block">{{ $error }}</p>
-    @endforeach
-</div>
+    <div class="controls">
+        @foreach ($errors as $error)
+            <p class="help-block">{{ $error }}</p>
+        @endforeach
+    </div>
 @endif
+
 <hr>
