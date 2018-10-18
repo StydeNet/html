831ee8d63bb1e8d573ec57a21b4f62f69e577575
Clemir Rondón <clemir@gmail.com>
2018-10-18 13:17:44 -0400

    Fix: don't include tags in the validations rules

--- tests/FormMakeCommandTest.php
+++ tests/FormMakeCommandTest.php
@@ -8,12 +8,7 @@ class FormMakeCommandTest extends TestCase
 {
     /** @test */
     function it_can_run_the_make_command()
-    {        
-        unlink(app_path('Http/Forms/UserForm.php'));
-        unlink(app_path('Http/Forms/FormModel.php'));
-        rmdir(app_path('Http/Forms'));
-        $this->assertDirectoryNotExists(app_path('Http/Forms'));
-
+    {
         $result = Artisan::call('make:form', ['name' => 'UserForm']);
 
         $this->assertDirectoryExists(app_path('Http/Forms'));
