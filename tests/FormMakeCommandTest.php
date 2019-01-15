05a5c8f9bdcc79889c83aa9b7866f10685693c6b
Clemir Rondón <clemir@gmail.com>
2019-01-15 18:38:01 -0400

    Add a test for make:menu command and fix the test for make:form command

--- tests/FormMakeCommandTest.php
+++ tests/FormMakeCommandTest.php
@@ -9,10 +9,28 @@ class FormMakeCommandTest extends TestCase
     /** @test */
     function it_can_run_the_make_command()
     {
+        $this->cleanDirectory();
+
         $result = Artisan::call('make:form', ['name' => 'UserForm']);
 
         $this->assertDirectoryExists(app_path('Http/Forms'));
         $this->assertFileExists(app_path('Http/Forms/UserForm.php'));
         $this->assertFileExists(app_path('Http/Forms/FormModel.php'));
+    }
+
+    protected function rmFile($filename)
+    {
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
+    }
+
+    protected function cleanDirectory()
+    {
+        $this->rmFile(app_path('Http/Forms/UserForm.php'));
+        $this->rmFile(app_path('Http/Forms/FormModel.php'));
+        if (is_dir(app_path('Http/Forms'))) {
+            rmdir(app_path('Http/Forms'));
+        }
     }
 }
